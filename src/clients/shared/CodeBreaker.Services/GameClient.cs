7f3ed672e6d256c9e99620c887205b6840b3695e
Christian Nagel <christian@christiannagel.com>
2023-02-24 18:06:21 +0100

    client networking library

---
+++ src/clients/shared/CodeBreaker.Services/GameClient.cs
@@ -0,0 +1,57 @@
+﻿using CodeBreaker.Shared.Api;
+using Microsoft.Extensions.Logging;
+
+using System.Net.Http.Json;
+
+namespace CodeBreaker.Services;
+
+public class GameClient : IGameClient, IGameReportClient
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly ILogger _logger;
+
+    public GameClient(HttpClient httpClient, ILogger<GameClient> logger)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+        _logger.LogInformation("Injected HttpClient with base address {uri} into GameClient", _httpClient.BaseAddress);
+    }
+
+    public async Task<CreateGameResponse> StartGameAsync(string username)
+    {
+        CreateGameRequest request = new(username);
+        HttpResponseMessage responseMessage = await _httpClient.PostAsJsonAsync("/games", request);
+        responseMessage.EnsureSuccessStatusCode();
+        return await responseMessage.Content.ReadFromJsonAsync<CreateGameResponse>();
+    }
+
+    public async Task<CreateMoveResponse> SetMoveAsync(Guid gameId, params string[] colorNames)
+    {
+        CreateMoveRequest request = new(colorNames.ToList());
+        HttpResponseMessage responseMessage = await _httpClient.PostAsJsonAsync($"/games/{gameId}/moves", request);
+        responseMessage.EnsureSuccessStatusCode();
+        return await responseMessage.Content.ReadFromJsonAsync<CreateMoveResponse>();
+    }
+
+    public async Task<GetGamesResponse?> GetGamesAsync(DateTime? date)
+    {
+        string requestUri = "/games";
+
+        date ??= DateTime.Now.Date;
+
+        requestUri = $"{requestUri}?date={date.Value:yyyy-MM-dd}";
+        _logger.LogInformation("Calling Codebreaker with {uri}", requestUri);
+
+        return await _httpClient.GetFromJsonAsync<GetGamesResponse>(requestUri);
+    }
+
+    public async Task<GetGameResponse?> GetGameAsync(Guid id)
+    {
+        string requestUri = $"/games/{id}";
+        _logger.LogInformation("Calling Codebreaker with {uri}", requestUri);
+        HttpResponseMessage responseMessage = await _httpClient.GetAsync(requestUri);
+        responseMessage.EnsureSuccessStatusCode();
+        return await responseMessage.Content.ReadFromJsonAsync<GetGameResponse>();
+    }
+}
---
+++ src/clients/shared/CodeBreaker.Services/IViewModelNavigationService.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeBreaker.Services;
+
+public interface IViewModelNavigationService
+{
+    bool NavigateToViewModel(string viewModelKey, object? parameter = null, bool clearNavigation = false);
+
+    bool NavigateToViewModel(Type viewModelType, object? parameter = default, bool clearNavigation = false) =>
+        NavigateToViewModel(viewModelType?.FullName ?? throw new ArgumentNullException(nameof(viewModelType)), parameter, clearNavigation);
+
+    bool GoBack();
+}
