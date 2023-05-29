14c1b209e39b981d35b41e9e37827535cebb0330
Christian Nagel <christian@christiannagel.com>
2023-05-29 06:54:33 +0200

    data client updates

--- src/CodeBreaker.Data.Demo/Program.cs
+++ src/CodeBreaker.Data.Demo/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Azure.Identity;
+
 using CodeBreaker.Data;
 using CodeBreaker.Data.Models;
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System.ComponentModel;
+using Microsoft.Extensions.Logging.Abstractions;
+
 using static CodeBreaker.Shared.Colors;
 
-string cosmosEndpoint = "https://codebreaker-db-account-563722.documents.azure.com:443/";
+string cosmosEndpoint = "https://codebreaker-db-account-thrive2023a1.documents.azure.com:443/";
 string databaseName = "codebreaker-db";
 //DefaultAzureCredential azureCredential = new();
 AzureCliCredential azureCredential = new();
@@ -14,13 +16,13 @@ AzureCliCredential azureCredential = new();
 var contextOptions = new DbContextOptionsBuilder<CodeBreakerContext>()
     .UseCosmos(cosmosEndpoint, azureCredential, databaseName)
     .Options;
-var dummyLogger = new LoggerFactory().CreateLogger<CodeBreakerContext>();
-using var context = new CodeBreakerContext(contextOptions, dummyLogger);
+var nullLogger = NullLogger<CodeBreakerContext>.Instance;
+using var context = new CodeBreakerContext(contextOptions, nullLogger);
 
 Game game = new() {
     GameId = Guid.NewGuid(),
     Code = new string[] { Red, Blue, Black, White },
-    Username = "Sebastian",
+    Username = "Christian",
     Start = DateTime.Now,
     Colors = Array.Empty<string>(),
     MaxMoves = 12,
