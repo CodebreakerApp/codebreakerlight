feade4b79cb691fd06c5f0f4d5a42bd6b0b99887
Christian Nagel <christian@christiannagel.com>
2023-09-27 11:16:37 +0200

    backend part 1

---
+++ part1-minimalapi/Codebreaker.GameAPIs.Models/Move.cs
@@ -0,0 +1,20 @@
+﻿namespace Codebreaker.GameAPIs.Models;
+
+public class Move(int moveNumber)
+{
+    /// <summary>
+    /// The move number for this move within the associated game.
+    /// </summary>
+    public int MoveNumber { get; } = moveNumber;
+
+    /// <summary>
+    /// The guess pegs from the user for this move.
+    /// </summary>
+    public required string[] GuessPegs { get; init; }
+    /// <summary>
+    /// The result from the analyer for this move based on the associated game that contains the move.
+    /// </summary>
+    public required string[] KeyPegs { get; init; }
+
+    public override string ToString() => $"{MoveNumber}. {string.Join(':', GuessPegs)}";
+}
---
+++ part1-minimalapi/Codebreaker.GameAPIs/Models/GameAPIModels.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Codebreaker.GameAPIs.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter<GameType>))]
+public enum GameType
+{
+    Game6x4,
+    Game6x4Mini,
+    Game8x5,
+    Game5x5x4
+}
+
+public record class CreateGameRequest(GameType GameType, string PlayerName);
+
+public record class CreateGameResponse(Guid GameId, GameType GameType, string PlayerName, int NumberCodes, int MaxMoves)
+{
+    public required IDictionary<string, IEnumerable<string>> FieldValues { get; init; }
+}
+
+public record class UpdateGameRequest(Guid GameId, GameType GameType, string PlayerName, int MoveNumber, bool End = false)
+{
+    public string[]? GuessPegs { get; set; }
+}
+
+public record class UpdateGameResponse(
+    Guid GameId,
+    GameType GameType,
+    int MoveNumber,
+    bool Ended,
+    bool IsVictory,
+    string[]? Results);
+
+public record GameSummary(Guid GameId, string PlayerName, DateTime StartTime, int NumberMoves, bool Won, TimeSpan? Duration);
