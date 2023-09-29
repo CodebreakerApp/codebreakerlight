281c8b2d044d410f24102a0be3216c3b871f76d0
Sebastian Szvetecz <sebastian@szvetecz.at>
2023-09-29 12:49:18 +0200

    Removed unused demo

--- LiveControlsDemo/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LiveControlsDemo;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- LiveControlsDemo/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace LiveControlsDemo;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
