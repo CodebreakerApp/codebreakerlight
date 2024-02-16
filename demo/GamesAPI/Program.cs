8a320e0abadad5f7bbf294e59ef6885d7703cba1
Christian Nagel <christian@christiannagel.com>
2024-02-16 10:21:52 +0100

    part 1 - games api

---
+++ demo/GamesAPI/Program.cs
@@ -0,0 +1,26 @@
+using Codebreaker.GameAPIs.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMetrics();
+
+builder.Services.AddSingleton<GamesMetrics>();
+
+builder.AddApplicationServices();
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGameEndpoints();
+
+app.Run();
+
