da81fa583f4370c1cece14c4e58cd703a9a2d248
Christian Nagel <christian@christiannagel.com>
2025-03-06 11:38:48 +0100

    02 aspire sample

---
+++ 02-final/AspireSample/AspireSample.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
